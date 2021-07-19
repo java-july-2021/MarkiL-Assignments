300075a6704eb4d8b38b6d1deb0d98c85755804c
MsMLyons <81724942+MsMLyons@users.noreply.github.com>
2021-07-18 23:24:59 -0400

    Java practice with the Pythagorean theorem

---
+++ 01-java-fundamentals/02-pythagorean-theorem/PythagoreanTest.java
@@ -0,0 +1,8 @@
+public class PythagoreanTest {
+    public static void main(String[] args) {
+        Pythagorean c = new Pythagorean();
+        System.out.println(c.calculateHypotenuse(2, 3));
+    }
+
+    
+}
