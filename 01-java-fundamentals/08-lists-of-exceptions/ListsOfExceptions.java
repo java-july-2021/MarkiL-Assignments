63b55572e22c008c60420f1d456f8abc2ff35051
MsMLyons <81724942+MsMLyons@users.noreply.github.com>
2021-07-18 23:43:54 -0400

    Create ListOfExceptions.java

---
+++ 01-java-fundamentals/08-lists-of-exceptions/ListsOfExceptions.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class ListsOfExceptions {
+    public static void main(String[] args){
+        ArrayList<Object> myList = new ArrayList<Object>();
+        myList.add("13");
+        myList.add("hello world");
+        myList.add(48);
+        myList.add("Goodbye World");
+
+        try{
+            for(int i = 0; i < myList.size(); i++) {
+                Integer castedValue = (Integer) myList.get(i);
+            }
+            System.out.println("This code worked!");
+        } catch (ClassCastException e){
+            System.out.println("Nope, this code did not work");
+        }
+    }
+}
